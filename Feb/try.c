6d618f48cc227562e40ac355fd9f4e6c5f9324c0
Vutha Soy <V010utha@gamil.com>
2024-02-13 11:45:43 +0700

    try

--- Feb/try.c
+++ Feb/try.c
@@ -1,57 +1,29 @@
-#include <stdio.h>
-
-int main() {
-    float basicSalary, allowance, additionalAllowance = 0, netSalary;
-    char grade, gender;
-    int age;
-    printf("Enter the basic salary: ");
-    scanf("%f", &basicSalary);
-    printf("Enter the grade (A, B, or C): ");
-    scanf(" %c", &grade);
-    printf("Enter the gender (M for male, F for female): ");
-    scanf(" %c", &gender);
-
-    printf("Enter the age: ");
-    scanf("%d", &age);
-    allowance = 0.1 * basicSalary;
-
-    switch(grade) {
-        case 'A':
-            additionalAllowance = 500;
-            break;
-        case 'B':
-            additionalAllowance = 300;
-            break;
-        case 'C':
-            additionalAllowance = 100;
-            break;
-        default:
-            printf("Error: Invalid grade entered.\n");
-           
-    }
-
-   
-
-   
-    if (gender == 'M' && age >= 40)
- {
-        additionalAllowance += 1000;
-    } 
-else if
- (gender == 'F' && age >= 35) 
+#include<stdio.h>
+int main()
 {
-        additionalAllowance += 800;
-    }
-
-
-
-    netSalary = basicSalary + allowance + additionalAllowance;
-
-  
-    printf("\nBasic Salary: $%.2f\n", basicSalary);
-    printf("Allowance: $%.2f\n", allowance);
-    printf("Additional Allowance: $%.2f\n", additionalAllowance);
-    printf("Net Salary: $%.2f\n", netSalary);
-
- 
+  char name[25];
+  int age;
+  float salary;
+  char new[10]="Newcomer";
+  printf("Enter your Name");
+  scanf("%c",name);
+   printf("\nEnter your age");
+  scanf(" %d",&age);
+   printf("\nEnter your salary");
+  scanf(" %f",&salary);  
+  printf("hello %s",new);
+  switch(new)
+  {
+    case 1 :
+    new=New;
+    printf("You have been selected");
+    break;
+    case 2 :
+    new=Old;
+    printf("You have have been disqualified");
+    break;
+  }
+  printf("Hello %c",name);
+  printf("Your salary is %f",salary);
+  printf("Your age is %d",age);
 }
